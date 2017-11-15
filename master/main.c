a3f1760d9aa5747f5863e4b63b861510da8dc5a2
Evan Zhao <etz3@cornell.edu>
2017-11-15 12:58:52 -0500

    adding wiring specs

--- master/main.c
+++ master/main.c
@@ -1,9 +1,31 @@
 /*
- * colorSensor.c
+ * main.c
  *
  * Created: 11/1/2017 2:12:29 PM
- * Author : CIT-Labs
+ * Author : Evan Zhao
  * 
+ * The master code for MAE something competition!
+ *
+ * 
+ * ************************************************
+ * Wiring configurations:
+ * ************************************************
+ * 
+ * Left Motor P0		->	PB2
+ * Left Motor P1		->	PB3
+ * Right Motor P0		->	PB4
+ * Right Motor P1		->	PB5
+ *
+ * --------------------------------
+ *
+ * Color Sensor Input	->	PB0
+ * Color Sensor PD5-PD8	->	S0-S3
+ *
+ * --------------------------------
+ *
+ * Random LED			->	PB1
+ *
+ * ************************************************
 */
 
 #define F_CPU 16000000UL
@@ -11,20 +33,26 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "serial.h"
+
 #define ABS(x) ((x > 0) ? x : -x)
-#define PRESCALER 1
 #define SET_PRESCALER() (TCCR1B |= 0B00000001)
-#define TOLERANCE 100
-#define TARGET_PERIOD 5000
+
 #define ALL 2
 #define LEFT 1
 #define RIGHT 0
 #define FORWARD 1
 #define BACKWARD 0
+
+#define PRESCALER 1
+#define BLUE_LOW 300
+#define BLUE_HIGH 500
+
+#define DEBUG 0
+
 int period = 0;
 int timer_value = 0;
 
-// Setting the motors. Assumes they are on PB2-5. Upper most is leftp0. then leftp1.
+// Setting the motors. Assumes they are on PB2-5. Upper most is leftp0. then leftp1. mirrored config for right.
 void initMotors() {
 	DDRB |= 0b00111100;
 }
@@ -68,57 +96,61 @@ void runRightMotors(int direction) {
 	}	
 }
 
-/* interrupt for color sensor output */
-ISR(PCINT0_vect) //interrupt for color sensor output
-{
-	timer_value = TCNT1 * PRESCALER;  //read timer1 with prescaler adjustment
+/* 
+ *Interrupt for color sensor output
+*/
+ISR(PCINT0_vect) {
+	timer_value = TCNT1;
 	TCNT1 = 0;   //reset timer
 }
 
-/* interrupt for color sensor and time */
-void initColor() //initializes the color sensor and timer
-{
-	DDRB       &= 0b11111110;     //set PB0 to input for color sensor output
-	PCICR      |=   0b00000001;     //enable PCI0
-	PCMSK0  |=   0b00000001;    //enable PCINT0 interrupt
+/* 
+ * Interrupt for color sensor and time
+ * Also initializes the color sensor and timer
+*/
+void initColor() {
+	DDRB	&=	0b11111110;		//set PB0 to input for color sensor output
+	PCICR	|=	0b00000001;		//enable PCI0
+	PCMSK0	|=	0b00000001;		//enable PCINT0 interrupt
 
-	DDRB     |=   0b00000010;   //set PB1 to output, for LED power output
-	PORTB   |=   0b00000010;   //set PB1 to high to return on color sensor LED
-	DDRD     |=   0b11110000;   //set PD5-PD8 to output for S0-S3 on color sensor
-	PORTD   |=   0b00010000;   //set PD5 to high for red filter 20 % scaling factor
+	DDRB	|=	0b00000010;		//set PB1 to output, for LED power output
+	PORTB	|=	0b00000010;		//set PB1 to high to return on color sensor LED
+	DDRD	|=	0b11110000;		//set PD5-PD8 to output for S0-S3 on color sensor
+	PORTD	|=	0b00010000;		//set PD5 to high for red filter 20 % scaling factor
 
-	TCCR1A = 0x0;   //set timer to normal operation
-	TCCR1B = 0x0;   //set timer to normal operation
-	SET_PRESCALER();   //set prescaler to 1
+	TCCR1A	=	0x0;			//set timer to normal operation
+	TCCR1B	=	0x0;			//set timer to normal operation
+	SET_PRESCALER();			//set prescaler to 1
 }
 
 
-/* reads color sensor. Returns an int. */
-int getColor()
-{
-	PCMSK0  |=   0b00000001;    //enable PCINT0 interrupt
-	_delay_ms(1);       //wait for ISR to calculate period. Max period is 250us < 1ms
-	PCMSK0 &= 0b11111110; //disable interrupt
-	return timer_value << 1;  //timer_value is half of period. double for full period
+/* 
+ * Reads color sensor. Returns an int.
+*/
+int getColor() {
+	PCMSK0 |= 0b00000001;						// enable PCINT0 interrupt
+	_delay_ms(1);								// wait for ISR to calculate period. Max period is 250us < 1ms
+	PCMSK0 &= 0b11111110;						// disable interrupt
+	int doubled = timer_value << 1;				// timer_value is half of period. double for full period
+	int measured_period = doubled * 0.004096;	// Scale the number of ticks we have counted by the amount of time each one is. 
+
+	return measured_period;
 }
 
 
 /* initializes the external LED output */
-void initLED()
-{
-	DDRB     |=   0b00000100;  //sets PB2 to output for LED
+void initLED() {
+	DDRB |= 0b00000100;  //sets PB2 to output for LED
 }
 
 /*turns on external LED */
-void enableLED()
-{
-	PORTB     |=   0b00000100;  //sets PB2 to high
+void enableLED() {
+	PORTB |= 0b00000100;  //sets PB2 to high
 }
 
 /*turns off external LED */
-void disableLED()
-{
-	PORTB     &=   0b11111011;  //sets PB2 to low
+void disableLED() {
+	PORTB &= 0b11111011;  //sets PB2 to low
 }
 
 void moveForwardAndBack() {
@@ -135,13 +167,21 @@ void moveForwardAndBack() {
 	_delay_ms(3000);
 }
 
+int isBlue(period) {
+	return period >= BLUE_LOW && period <= BLUE_HIGH;
+}
+
 void readColor() {
 	period = getColor();    //read color sensor
-	printf("Period: %u[microseconds]\n", period/16);
-	if (ABS(period - TARGET_PERIOD) < TOLERANCE) {
-		disableLED();
-	} else {
+
+	if(DEBUG) {
+		printf("Period: %u[microseconds]\n", period/16);
+	}
+
+	if(isBlue) {
 		enableLED();
+	} else {
+		disableLED();
 	}
 }
 
